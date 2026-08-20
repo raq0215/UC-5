40ea963262de4bc2b36005df09c24c85086e757b
Raquel Moreira <raquelmoreirasilva0215@gmail.com>
2026-08-19 21:00:53 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("JavaScript conectado!");
+//console.log("Document");
+
+const titulo = document.querySelector("h1");
+console.log(titulo);
+
+//const hero = document.querySelector("#hero");
+//console.log(hero.textContent);
+
+
+//const linkSocial = document.querySelector(".link-social");
+//console.log(link-social);
+//console.log(linkSocial.textContent);
+
+
+//const titulo = document.querySelector("h1");
+//titulo.textContent = "Raquel Moreira | Desenvolvedora Web";
+
+//const hero = document.querySelector("#hero");
+//const apresentacao = hero.querySelector("p");
+//apresentacao.textContent =
+    //"Olá! Sou Raquel e estou começando minha jornada no desenvolvimento web.";
+
+//titulo.style.color = "red";
+//hero.style.backgroundColor = "lightblue";
+
+//document.getElementById("hero");
+//console.log(hero)
