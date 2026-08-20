0ae03b5b718064b12fb5eae63308ca917be52011
Raquel Moreira <raquelmoreirasilva0215@gmail.com>
2026-08-20 20:21:06 -0300

    Disable button click event listener
    Comment out the event listener for the button click.

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ console.log(titulo);
 // Ao clicar no botão aparece o texto no console log
 //const botaoSobre = document.getElementById("botao-sobre");
 
-botaoSobre.addEventListener("click", function () {
+//botaoSobre.addEventListener("click", function () {
 //    console.log("O botão foi clicado!");
 // });
 
